ab857e40008db990e02eb54169858753129b564f
SD-github21 <sapandonde.gmail.com>
2022-06-02 22:58:51 -0500

    organized SELECT SQL queries into corresponding functions

--- index.js
+++ index.js
@@ -3,23 +3,6 @@ const inquirer = require("inquirer");
 const cTable = require("console.table");
 const db = require("./db/connection");
 
-let view_depts = `SELECT department.id AS dept_id, department.name AS dept_name FROM department`;
-
-let view_roles = `SELECT roles.id AS role_id, roles.title AS job_title, department.name AS dept_name,
-                 roles.salary AS salary
-                 FROM roles
-                 INNER JOIN department ON roles.department_id = department.id`;
-
-let view_emps = `SELECT employee.id AS emp_id, employee.first_name AS first_name, employee.last_name AS last_name,  
-                roles.title AS job_title, department.name AS department, roles.salary,
-                CONCAT(employee.first_name,' ', employee.last_name) as manager
-                FROM employee
-                INNER JOIN roles ON employee.role_id = roles.id
-                LEFT JOIN department ON department.id = roles.department_id`; 
-
-let add_dept = `INSERT INTO department (name)
-                VALUES
-                (?)`;
 let add_role = `INSERT INTO roles (title, salary, department_id)
                 VALUES
                 (?, ?, ?)`;
@@ -39,7 +22,8 @@ const profileMenuOptions = () => {
             name: "menu",
             message: "What would you like to do?",
             choices: ['View all departments', 'View all roles', 'View all employees', 
-                      'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
+                      'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 
+                      'Exit'],
         })
     .then (({ menu }) => {
         // Create if statement to direct user to the SQL queries associated with the menu options
@@ -69,18 +53,25 @@ const profileMenuOptions = () => {
 
 const viewDepts = () => {
 
-db.connect(function(err) {
-    if (err) throw err;
-    db.query(view_depts, function (err, result) {
-      if (err) throw err;
-      console.table(result);
-      profileMenuOptions();
+    const view_depts = `SELECT department.id AS dept_id, department.name AS dept_name FROM department`;
+
+    db.connect(function(err) {
+        if (err) throw err;
+        db.query(view_depts, function (err, result) {
+            if (err) throw err;
+            console.table(result);
+            profileMenuOptions();
+        });
     });
-  });
 };
 
 const viewRoles = () => {
 
+    const view_roles = `SELECT roles.id AS role_id, roles.title AS job_title, department.name AS dept_name,
+    roles.salary AS salary
+    FROM roles
+    INNER JOIN department ON roles.department_id = department.id`;
+    
     db.connect(function(err) {
         if (err) throw err;
         db.query(view_roles, function (err, result) {
@@ -93,6 +84,13 @@ const viewRoles = () => {
     
 const viewEmps = () => {
 
+    const view_emps = `SELECT employee.id AS emp_id, employee.first_name AS first_name, employee.last_name AS last_name,  
+    roles.title AS job_title, department.name AS department, roles.salary,
+    CONCAT(employee.first_name,' ', employee.last_name) as manager
+    FROM employee
+    INNER JOIN roles ON employee.role_id = roles.id
+    LEFT JOIN department ON department.id = roles.department_id`; 
+
     db.connect(function(err) {
         if (err) throw err;
         db.query(view_emps, function (err, result) {
@@ -103,6 +101,9 @@ const viewEmps = () => {
         });
     };
 
+
+    
+    
 
 
 profileMenuOptions();
